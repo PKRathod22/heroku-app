255f67ec767cbc705d23cc4cc19e1d58c1ac9a49
Praveen <praveenjohn.smith@gmail.com>
2018-02-04 15:01:39 +0530

    user

--- src/main/java/com/pk/model/UserMaster.java
+++ src/main/java/com/pk/model/UserMaster.java
@@ -9,8 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class UserMaster implements Serializable {
 	
 	private static final long serialVersionUID = -6513828693331958787L;
